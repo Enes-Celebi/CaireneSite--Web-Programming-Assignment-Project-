365eef4833259c50511450df46079f76a5d13553
Enes-Celebi <121902027+Enes-Celebi@users.noreply.github.com>
2023-01-06 19:54:14 +0300

    Done!
    Spent a lot of time creating multiple backups, and ending up completing one of them so I pasted it in the directory
    
    * added pictures
    * filled database
    * completed frontend

---
+++ AdminWeb/AdminWeb/Controllers/EmployeeController.cs
@@ -0,0 +1,104 @@
+﻿//using system;
+//using adminweb.models;
+//using system.collections.generic;
+//using system.linq;
+//using system.web;
+//using system.web.mvc;
+
+//namespace adminweb.controllers
+//{
+//    public class employeecontroller : controller
+//    {
+//        dbentities db = new dbentities();
+
+//        public actionresult index()
+//        {
+//            return view(db.admin_employee.tolist());
+//        }
+
+//        public actionresult create()
+//        {
+//            return view();
+//        }
+
+//        [httppost]
+//        public actionresult create(admin_employee emp)
+//        {
+//            if (modelstate.isvalid)
+//            {
+//                db.admin_employee.add(emp);
+//                db.savechanges();
+//                return redirecttoaction("index");
+//            }
+
+//            return view();
+//        }
+
+//        //get edit
+//        [httpget]
+//        public actionresult edit(int id)
+//        {
+//            admin_employee emp = db.admin_employee.single(x => x.empid == id);
+//            if (emp == null)
+//            {
+//                return httpnotfound();
+//            }
+//            //viewbag.roleid = new selectlist(db.roles, "roleid", "rolename", emp.roleid);
+//            return view("edit", emp);
+//        }
+
+//        //post edit
+//        [httppost]
+//        public actionresult edit(admin_employee emp)
+//        {
+//            if (modelstate.isvalid)
+//            {
+//                db.entry(emp).state = entitystate.modified;
+//                db.savechanges();
+//                return redirecttoaction("index");
+//            }
+//            //viewbag.roleid = new selectlist(db.roles, "roleid", "rolename", emp.roleid);
+//            return view(emp);
+//        }
+
+//        //get details
+//        public actionresult details(int id)
+//        {
+//            admin_employee emp = db.admin_employee.find(id);
+//            if (emp == null)
+//            {
+//                return httpnotfound();
+//            }
+//            return view(emp);
+//        }
+
+//        //get delete
+//        public actionresult delete(int id)
+//        {
+//            admin_employee emp = db.admin_employee.find(id);
+//            if (emp == null)
+//            {
+//                return httpnotfound();
+//            }
+//            return view(emp);
+
+//        }
+
+//        //post delete confirmed
+//        [httppost, actionname("delete")]
+//        [validateantiforgerytoken]
+//        public actionresult deleteconfirmed(int id)
+//        {
+//            admin_employee admin_employee = db.admin_employee.find(id);
+//            db.admin_employee.remove(admin_employee);
+//            db.savechanges();
+//            return redirecttoaction("index");
+//        }
+
+//        protected override void dispose(bool disposing)
+//        {
+//            base.dispose(disposing);
+//        }
+
+//    }
+//}
